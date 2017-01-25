c84e5d043e42d8a3f6316b57c9987ff1f6b0aa0d
Dainiulis <dainiusmie@gmail.com>
2017-01-25 21:35:00 +0200

    pridejau fragmentus, bet crashina jei veikiant timeriui pereinu i kita fragmenta ir negriztu. Reikia sugalvoti iseiti nes kai replacinu i kita fragmenta TimerFragmenta detach() ir del to crashina.

---
+++ app/src/main/java/com/dmiesoft/fitpomodoro/exercisesFragments/ExerciseGroupFragment.java
@@ -0,0 +1,31 @@
+package com.dmiesoft.fitpomodoro.exercisesFragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.dmiesoft.fitpomodoro.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ExerciseGroupFragment extends Fragment {
+
+
+    public ExerciseGroupFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_exercise_group, container, false);
+    }
+
+}
