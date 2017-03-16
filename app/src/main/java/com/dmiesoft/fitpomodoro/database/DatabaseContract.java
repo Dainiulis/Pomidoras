7ca38681a3a11157bd45050f06d26901c8258512
Dainiulis <dainiusmie@gmail.com>
2017-03-16 17:01:41 +0200

    now favorites get added

--- app/src/main/java/com/dmiesoft/fitpomodoro/database/DatabaseContract.java
+++ app/src/main/java/com/dmiesoft/fitpomodoro/database/DatabaseContract.java
@@ -4,13 +4,14 @@ import android.provider.BaseColumns;
 
 public class DatabaseContract {
 
-    public static final int DATABASE_VERSION = 2;
+    public static final int DATABASE_VERSION = 5;
     public static final String DATABASE_NAME = "pomidoras.db";
     private static final String TEXT = " TEXT";
     private static final String COMMA_SEP = ",";
     public static final String CREATE_TABLE_STRING = "CREATE TABLE ";
 
-    private DatabaseContract() {}
+    private DatabaseContract() {
+    }
 
     public static abstract class ExercisesTable implements BaseColumns {
         public static final String TABLE_NAME = "exercises";
@@ -62,6 +63,22 @@ public class DatabaseContract {
                         _ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                         COLUMN_NAME + TEXT + COMMA_SEP +
                         COLUMN_DATE + " TIMESTAMP NOT NULL DEFAULT current_timestamp" +
+                        ");";
+        public static final String DELETE_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
+    }
+
+    public static abstract class FavExIdsTable {
+        public static final String TABLE_NAME = "favorites_exercises_ids";
+        public static final String COLUMN_EXERCISE_ID = "exercise_id";
+        public static final String COLUMN_FAVORITE_ID = "favorite_id";
+
+        public static final String CREATE_TABLE =
+                CREATE_TABLE_STRING + TABLE_NAME + " (" +
+                        COLUMN_EXERCISE_ID + " INTEGER," +
+                        COLUMN_FAVORITE_ID + " INTEGER," +
+                        " FOREIGN KEY(" + COLUMN_EXERCISE_ID + ") REFERENCES " + ExercisesTable.TABLE_NAME + "(" + ExercisesTable._ID + ") ON DELETE CASCADE" +
+                        " FOREIGN KEY(" + COLUMN_FAVORITE_ID + ") REFERENCES " + FavoritesTable.TABLE_NAME + "(" + FavoritesTable._ID + ") ON DELETE CASCADE" + COMMA_SEP +
+                        " PRIMARY KEY(" + COLUMN_EXERCISE_ID + COMMA_SEP +  COLUMN_FAVORITE_ID + ")" +
                         ");";
         public static final String DELETE_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
     }
