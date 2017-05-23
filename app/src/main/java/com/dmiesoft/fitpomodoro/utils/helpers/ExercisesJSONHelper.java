2c0f0fa2f0e20d2aec90399aa36041887ff6cbd7
Dainiulis <dainiusmie@gmail.com>
2017-05-23 08:38:17 +0300

    Before adding average reps

---
+++ app/src/main/java/com/dmiesoft/fitpomodoro/utils/helpers/ExercisesJSONHelper.java
@@ -0,0 +1,11 @@
+package com.dmiesoft.fitpomodoro.utils.helpers;
+
+/**
+ * Created by daini on 2017-05-20.
+ */
+
+public class ExercisesJSONHelper {
+
+    private static final String TAG = "EJSONH";
+
+}
